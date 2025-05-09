b4275556369396da73b004617d0f302140ce1169
maksym rozvodovskyi <maksym.rozvodovskyi@ukr.net>
2025-05-09 16:53:35 +0300

    save commit

---
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -0,0 +1,13 @@
+import css from "./ImageGallery.module.css";
+import ImageCard from "./ImageCard/ImageCard";
+
+export default function ImageGallery() {
+  <ul>
+    {/* Набір елементів списку із зображеннями */}
+    <li>
+      <div>
+        <img src="" alt="" />
+      </div>
+    </li>
+  </ul>;
+}
---
+++ src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,17 @@
+import css from "./SearchBar.module.css";
+
+export default function SearchBar({ onSubmit }) {
+  return (
+    <header>
+      <form>
+        <input
+          type="text"
+          autocomplete="off"
+          autofocus
+          placeholder="Search images and photos"
+        />
+        <button type="submit">Search</button>
+      </form>
+    </header>
+  );
+}
