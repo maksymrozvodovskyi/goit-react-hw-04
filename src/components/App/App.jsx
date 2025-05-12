473286dd292b277827ab084e6b916f78750fc015
maksym rozvodovskyi <maksym.rozvodovskyi@ukr.net>
2025-05-12 14:18:23 +0300

    save commit

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,21 +1,23 @@
 import css from "./App.module.css";
+import axios from "axios";
+
+import { useState } from "react";
+import { fetchData } from "../../unsplash-api";
 
 import SearchBar from "../SearchBar/SearchBar";
 import ImageGallery from "../ImageGallery/ImageGallery";
-import { useState } from "react";
-import axios from "axios";
-import { fetchData } from "../../unsplash-api";
 
 export default function App() {
   const [images, setImages] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleSearch = async (newImage) => {
     try {
       if (newImage === "") {
         return;
       }
+      setIsLoading(true);
       const data = await fetchData(newImage);
-
       setImages(data);
     } catch (error) {
       console.log(error);
@@ -25,6 +27,7 @@ export default function App() {
   return (
     <div>
       <SearchBar onSubmit={handleSearch} />
+      {isLoading && <p>Loading...</p>}
       {images.length > 0 && <ImageGallery data={images} />}
     </div>
   );
