55a592aecdfd1745fbbde3c92f5b1026afed7db7
maksym rozvodovskyi <maksym.rozvodovskyi@ukr.net>
2025-05-12 14:47:46 +0300

    save commit

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -6,29 +6,37 @@ import { fetchData } from "../../unsplash-api";
 
 import SearchBar from "../SearchBar/SearchBar";
 import ImageGallery from "../ImageGallery/ImageGallery";
+import Loader from "../Loader/Loader";
+import ErrorMessage from "../ErrorMessage/ErrorMessage";
 
 export default function App() {
   const [images, setImages] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
 
   const handleSearch = async (newImage) => {
     try {
       if (newImage === "") {
         return;
       }
+      setIsError(false);
+      setImages([]);
       setIsLoading(true);
       const data = await fetchData(newImage);
       setImages(data);
-    } catch (error) {
-      console.log(error);
+    } catch {
+      setIsError(true);
+    } finally {
+      setIsLoading(false);
     }
   };
 
   return (
     <div>
       <SearchBar onSubmit={handleSearch} />
-      {isLoading && <p>Loading...</p>}
       {images.length > 0 && <ImageGallery data={images} />}
+      {isError && <ErrorMessage />}
+      {isLoading && <Loader />}
     </div>
   );
 }
